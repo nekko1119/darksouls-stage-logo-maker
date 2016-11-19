fdd466a9bcdaa41ffd245b709f262bcb5250c7b8
nekko1119 <taku50501119@gmail.com>
2016-11-20 03:46:37 +0900

    ローカル画像を選択してプレビュー表示する

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,62 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
-class App extends React.Component<any, any> {
+interface State {
+    dataUrl?: string
+};
+
+const Preview = ({dataUrl}: {dataUrl?: string}) => (
+    dataUrl ? <img src={dataUrl} width={480} /> : null!
+);
+
+class App extends React.Component<void, State> {
+
+    static inputId = "upload-file";
+
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    private handleImageSelect = (e: any) => {
+        const image = e.target.files[0] as File;
+        if (!image) {
+            return;
+        }
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            this.setState({
+                dataUrl: reader.result
+            });
+        };
+        reader.readAsDataURL(image.slice(0, image.size, image.type));
+    };
+
+    private forwardImageSelect = () => {
+        const element = document.getElementById(App.inputId);
+        element!.click();
+    };
+
     render() {
         return (
-            <div>
-                <button type="button" className="btn btn-primary">Primary</button>
+            <div className="container">
+                <h1>DARK SOULS風ステージロゴを画像に重ねる</h1>
+                <input
+                    id={App.inputId}
+                    type="file"
+                    onChange={this.handleImageSelect}
+                    style={{ display: "none" }}
+                    accept="image/*"
+                />
+                <button
+                    type="button"
+                    className="btn btn-default"
+                    onClick={this.forwardImageSelect}
+                >
+                    画像を選択
+                </button>
                 <p>YOU DIED</p>
+                <Preview dataUrl={this.state.dataUrl} />
             </div>
         );
     }
@@ -15,6 +65,4 @@ class App extends React.Component<any, any> {
 
 const element = document.getElementById("app");
 
-if (element !== null) {
-    ReactDOM.render(<App />, element);
-}
+ReactDOM.render(<App />, element!);
